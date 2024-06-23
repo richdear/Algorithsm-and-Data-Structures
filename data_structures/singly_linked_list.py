4d70cd94a2d0d4575ad01a4d566699a3946bb098
a.bakiyev@iqfulfillment.com <a.bakiyev@iqfulfillment.com>
2024-06-23 11:28:20 +0100

    add typing, refactor

--- data_structures/singly_linked_list.py
+++ data_structures/singly_linked_list.py
@@ -1,20 +1,17 @@
 class Node():
-    def __init__(self,value):
-        self.value=value
-        self.next=None
+    def __init__(self,value: any):
+        self.value: any =value
+        self.next: Node| None = None
     
-
-
-
 class SinglyLinkedList():
     def __init__(self):
-        self.length=0
-        self.head=None
-        self.tail=self.head
+        self.length:int=0
+        self.head: Node|None =None
+        self.tail: Node|None =self.head
     
-    def push(self,value):
+    def push(self,value: any):
         new_node=Node(value)
-        if self.head==None:
+        if self.empty():
             self.head=new_node
             self.tail=self.head
         else:
@@ -22,9 +19,24 @@ class SinglyLinkedList():
             self.tail=new_node
         self.length+=1
     
+    def pop(self) -> any:
+        if self.empty():
+            return None
+        else:
+            pointer=self.head
+            prev=None
+            while pointer.next!=None:
+                prev=pointer
+                pointer=pointer.next
+            prev.next=None
+            popped_value=pointer.value
+            self.tail=prev
+            self.length-=1
+            return popped_value
+    
     def __str__(self) -> str:
-        pointer=self.head
-        label=""
+        pointer:Node=self.head
+        label:str=""
         while pointer.next!=None:
             label+=f"{pointer.value} "
             pointer=pointer.next
@@ -33,13 +45,19 @@ class SinglyLinkedList():
     
     def __len__(self):
         return self.length
+    
+    def empty(self)->bool:
+        return self.head==None
 
 link_list=SinglyLinkedList()
 link_list.push("Teste")
 link_list.push("12121")
 link_list.push(78)
 link_list.push(1)
+link_list.pop()
+link_list.pop()
 print(link_list)
 print(len(link_list))
+
 
 
